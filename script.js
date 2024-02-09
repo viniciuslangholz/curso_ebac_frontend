2df07be9185113033c4e882649e2ec194182972f
Vinícius Langholz <viniciuslangholz@gmail.com>
2024-02-09 13:05:53 -0300

    adicionando arquivos da atividade
    criado um formulário utilizando jquery

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+
+    $('#cadastroForm').submit(function(event){
+
+        event.preventDefault();
+        alert('Formulário enviado com sucesso!');
+    });
+});
